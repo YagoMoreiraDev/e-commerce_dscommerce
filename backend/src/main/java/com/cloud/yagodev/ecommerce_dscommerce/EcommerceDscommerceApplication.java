44bd1ee581abf3de25b31dd43a65bbae91a29f21
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-05-31 13:55:52 -0300

    Adicionando configuraçoes e pacotes necessários para inicio do projeto

---
+++ backend/src/main/java/com/cloud/yagodev/ecommerce_dscommerce/EcommerceDscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.ecommerce_dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDscommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cloud/yagodev/ecommerce_dscommerce/EcommerceDscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.ecommerce_dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
